9d8f1919eb106f4d9b9ae090232af5ffed123a1b
Mark Green Jr <mrgssg@gmail.com>
2023-02-20 18:17:22 -0500

    fixed server.js and css issues

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require ('path');
 const uuid = require ('./helpers/uuid');
 const {getNotes, savedNotes} = require('./helpers/notesH');
 
-const Port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -14,11 +14,12 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}))
 
 // GET requests
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
 
 app.get('/api/notes', (req, res) => {res.json(getNotes());
 });
@@ -26,7 +27,7 @@ app.get('/api/notes', (req, res) => {res.json(getNotes());
 // post reqs
 app.post('/api/notes', (req, res) => { 
     currentNotes = getNotes(); 
-});
+
 
 // new note instructions
 const {title, text} = req.body;
@@ -44,6 +45,7 @@ if (title && text) {
      else {
         res.json('Error, please try to save again.')
      };
+    });
 
 // DELETE option 
 app.delete('/api/notes/:id', (req, res) => {
